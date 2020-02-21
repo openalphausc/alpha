f9f2c6a69a6c1d43949987dee6b725e10eaf3e54
Colin Spiridonov <spiridon@usc.edu>
2020-02-21 12:56:25 -0800

    Added body and wiper and implemented mouse control for wiper

---
+++ Assets/Scripts/MudCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudCollide : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.name == "Wiper" && Input.GetKey("space"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.07f;
+    }
+
+    void OnCollisionEnter2D(Collision2D dataFromCollision)
+    {
+
+        string name = dataFromCollision.gameObject.name;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float moveX = 0;
+      float moveY = 0;
+      if(Input.GetKey("d")) moveX = speed;
+      else if(Input.GetKey("a")) moveX = -speed;
+      transform.position = new Vector3(transform.position.x + moveX, transform.position.y + moveY, transform.position.z);
+    }
+}
