b3afacbedd202b3dc1d5b995d9832b1d517a93e6
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-03-03 19:20:11 -0800

    Implemented Gravity

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public float movementSpeed;
+
+    public Rigidbody rb;
+
+    public LayerMask platform;
+
+    public float jumpForce = 7;
+
+    private bool inAir;
+
+    public BoxCollider col;
+
+
     void Start()
     {
-        
+        inAir = false;
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
     }
 
     void Update()
@@ -16,9 +30,26 @@ public class CharacterMover : MonoBehaviour
         {
             transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
+        }
+        //utilizing the addforce function gives the fall a more realistic feel
+        if (Input.GetKey(KeyCode.W) && !inAir)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            inAir = true;
+        }
+
+    }
+    void OnCollisionEnter(Collision dataFromCollision)
+    {
+
+        string name = dataFromCollision.gameObject.name;
+        Debug.Log("name: " + name);
+        if (name == "Platform")
+        {
+            inAir = false;
         }
     }
 }
