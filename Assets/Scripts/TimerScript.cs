47066d7e52e7ce32b51837b968bada1d3be42e9b
cmzhang526 <cmzhang@usc.edu>
2020-03-04 18:41:36 -0800

    Countup (?)

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,32 +6,21 @@ using UnityEngine;
 public class TimerScript : MonoBehaviour
 {
     [SerializeField] private Text uiText;
-    [SerializeField] private float mainTimer;
 
     private float timer;
-    private bool canCount = true;
-    private bool doOnce = false;
 
     void Start()
     {
-        timer = mainTimer;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer>= 0.0f && canCount)
+        if (timer<= 10.0f)
         {
-            timer -= Time.deltaTime;
+            timer += Time.deltaTime;
             uiText.text = timer.ToString("F");
-        }
-
-        else if (timer <= 0.0f && !doOnce)
-        {
-            canCount = false;
-            doOnce = true;
-            uiText.text = "0.00";
-            timer = 0.0f;
         }
         
     }
