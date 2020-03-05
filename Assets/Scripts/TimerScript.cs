69136f514f0f04e39293b020962107f18d717587
cmzhang526 <cmzhang@usc.edu>
2020-03-04 18:15:23 -0800

    Countdown Timer and Clock Graphic
    Will change to countup

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    void Start()
+    {
+        timer = mainTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer>= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.0f;
+        }
+        
+    }
+}
