16946a44fbb0acd7be18ad82a6407b0276e18775
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-03-27 19:36:16 -0500

    Implements a button that appears after a variable amount of time.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/GoHome.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoHome : MonoBehaviour
+{
+    public float time;
+    public GameObject parent;
+    public GameObject myPrefab;
+    private float current;
+    private bool once;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0.0f;
+        once = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current += Time.deltaTime;
+        if (current > time && once == false)
+        {
+            GameObject button = Instantiate(myPrefab) as GameObject;
+            button.transform.position = new Vector3(parent.transform.position.x - 14.0f, parent.transform.position.y, 0.0f);
+            once = true;
+        }
+    }
+}
