1941242f7236874a505ac58da5b5fe386e093213
Owen Mech <mech.owen@gmail.com>
2020-03-27 21:27:54 -0500

    Moved GoHome to Bottom Left

--- Assets/Scripts/GoHome.cs
+++ Assets/Scripts/GoHome.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class GoHome : MonoBehaviour
 {
     public float time;
-    public GameObject parent;
     public GameObject myPrefab;
     private float current;
     private bool once;
@@ -23,7 +22,6 @@ public class GoHome : MonoBehaviour
         if (current > time && once == false)
         {
             GameObject button = Instantiate(myPrefab) as GameObject;
-            button.transform.position = new Vector3(parent.transform.position.x - 14.0f, parent.transform.position.y, 0.0f);
             once = true;
         }
     }
