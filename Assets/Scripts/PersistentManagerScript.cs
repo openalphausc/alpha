be59ee061e63a8a8e2ac43128e73eee31df9a340
Charlie Feuerborn <cfeuerbo@usc.edu>
2020-04-06 23:38:17 -0700

    Working on persistentManager singletons

---
+++ Assets/Scripts/PersistentManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PersistentManagerScript : MonoBehaviour
+{
+    public static PersistentManagerScript Player {get; private set;}
+
+    public int money;
+
+    private void Awake()
+    {
+        if (Player == null){
+            Player = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AttemptPurchase()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/ShopManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManagerScript : MonoBehaviour
+{
+    public Text MoneyTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoneyTxt.text = PersistentManagerScript.Player.money.ToString();
+    }
+
+    
+    public void AttemptPurchase()
+    {
+        SceneManager.LoadScene("first");
+    }
+}
