7a824acbd05eacfd8f2c75b4173ef8880b9bc575
Charlie Feuerborn <cfeuerbo@usc.edu>
2020-04-11 18:57:49 -0700

    Applied uniform code formatting

--- Assets/Scripts/PersistentManagerScript.cs
+++ Assets/Scripts/PersistentManagerScript.cs
@@ -6,29 +6,29 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
-
 public class PersistentManagerScript : MonoBehaviour
 {
     //Treat "Instance" as the equivalent of "player" here. Player.Buy(item) == Instance.Buy(item).
-    public static PersistentManagerScript Instance {get; private set;}
-    
+    public static PersistentManagerScript Instance { get; private set; }
+
     //Keeps track of the player's current total currency.
     //think about edge cases when editing this directly:
     //If a player starts a round, wipes some stuff, and exits to the title screen or quits the game, restarts the level,
     //etc
     public int money;
-    
+
     //A vector of items which holds all the items the player has
     //should make this private in the future, but useful to be public for debugging purposes in the editor
     [SerializeField] List<Item> inventory;
     //Consider adding other items such as record stats for display at end of rounds or in title "stats" screen
-    
+
     //This runs when the presistentManagerScript is first run, before "Start()" of all items, so that it will be non-nul
     //if called early in a scene
     private void Awake()
     {
         //If there isn't an instance yet, create one
-        if (Instance == null){
+        if (Instance == null)
+        {
             Instance = this;
             //and ensure it won't destroy when you load another scene
             DontDestroyOnLoad(gameObject);
@@ -39,35 +39,38 @@ public class PersistentManagerScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     /**Buy function: Input the item to the inventory if player can afford it AND doesn't already have the item. Reduce
      * money by the item's price.
      * Returns false _if the item is already in the inventory_ or _can't afford_
      * Returns true if the purchase is successful
      */
-    public bool Buy(Item item){
+    public bool Buy(Item item)
+    {
         if (inventory.Contains(item))
         {
             return false;
         }
+
         if (money < item.price)
         {
             return false;
         }
-        
+
         print("Successful buy!");
         money -= item.price;
         inventory.Add(item);
         return true;
-
     }
 
-    
+
     //~~~~~~~~~~~~~~~~~~Modifiers section~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
     /**
      * Returns true if the player has a watch or an item that acts as a watch.
      * 
      */
-    public bool InvWatch(){
+    public bool InvWatch()
+    {
         var watch = false;
 
         foreach (var item in inventory)
@@ -81,12 +84,13 @@ public class PersistentManagerScript : MonoBehaviour
 
         return watch;
     }
-    
+
     /**
      * Returns true if the player has a watch or an item that acts as a watch.
      * 
      */
-    public bool InvHeadlamp(){
+    public bool InvHeadlamp()
+    {
         var headlamp = false;
 
         foreach (var item in inventory)
@@ -100,13 +104,14 @@ public class PersistentManagerScript : MonoBehaviour
 
         return headlamp;
     }
-    
+
     /**
      * Returns an int, the total percentage speed increase players
      * should receive from all their items.
      * 
      */
-    public int InvSpeedBonus(){
+    public int InvSpeedBonus()
+    {
         var sum = 0;
 
         foreach (var item in inventory)
@@ -134,7 +139,7 @@ public class PersistentManagerScript : MonoBehaviour
 
         return sum;
     }
-    
+
     /**
      * Returns an int, the number of bonus sprays the player has from all their items.
      */
@@ -150,7 +155,7 @@ public class PersistentManagerScript : MonoBehaviour
 
         return sum;
     }
-    
+
     /**
      * Returns a double, the amount range increase the player has from all their items (default range is not yet set,
      * but will probably be approximately 10-15 % of the bar.
@@ -182,10 +187,10 @@ public class PersistentManagerScript : MonoBehaviour
             print("Adding the time reductions " + item.timePenalties + " of " + item.name);
             sum += item.timePenalties;
         }
-        
+
         return sum;
     }
-    
+
     /**
      * Returns a double, the wipe range increase (default range is 3, max arm length 4)
      * the player has from all their items.
@@ -201,10 +206,10 @@ public class PersistentManagerScript : MonoBehaviour
             print("Adding the arm range increase " + item.rangeIncrease + " of " + item.name);
             sum += item.rangeIncrease;
         }
-        
+
         return sum;
     }
-    
+
     /**
      * Returns an int, the length of day increase the player has from all their items.
      */
@@ -217,10 +222,10 @@ public class PersistentManagerScript : MonoBehaviour
             print("Adding the time increase " + item.timeIncrease + " of " + item.name);
             sum += item.timeIncrease;
         }
-        
+
         return sum;
     }
-    
+
     /**
      * Returns an int, the percentage income per wipe increase the player has from all their items. Apply in
      * the start function of Wiper Controller in the assignment of incomePerWipe (feat-cash-shit branch)
@@ -234,10 +239,10 @@ public class PersistentManagerScript : MonoBehaviour
             print("Adding the income increase " + item.incomeIncrease + " of " + item.name);
             sum += item.incomeIncrease;
         }
-        
+
         return sum;
     }
-    
+
     /**
      * Returns an int, the total bonus money the player gets for completion of a building
      * that the player has from all their items. 
@@ -251,9 +256,9 @@ public class PersistentManagerScript : MonoBehaviour
             print("Adding the income increase " + item.bonusOnCompletionIncrease + " of " + item.name);
             sum += item.bonusOnCompletionIncrease;
         }
-        
+
         return sum;
     }
-    
+
     //#########################~~~~~END OF MODIFIERS SECTION~~~~~~~~##################################################//
 }
