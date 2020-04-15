7188745ff2b59e95cb7ffab4c7aa70b192619e70
Colin Spiridonov <colinspiri@gmail.com>
2020-04-14 21:19:25 -0700

    Made gauges persistent

--- Assets/Scripts/GaugeMove.cs
+++ Assets/Scripts/GaugeMove.cs
@@ -26,22 +26,15 @@ public class GaugeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if(inputHandler.refilling[fluidIndex] || (decreasing && sprayController.animating)) {
-        // gauge shows up when player is spraying or refilling
-        // x and y are between 0 and cam.pixelWidth and cam.pixelHeight
-        float x = 0.01f * cam.pixelWidth;
-        if(gameObject.name == "FluidGaugeJ") x *= 9f; // percents across the screen
-        if(gameObject.name == "FluidGaugeK") x *= 12f;
-        else if(gameObject.name == "FluidGaugeL") x *= 15f;
-        float y = 10f * 0.01f * cam.pixelHeight;
-        float z = 5f;
+      // x and y are between 0 and cam.pixelWidth and cam.pixelHeight
+      float x = 0.01f * cam.pixelWidth;
+      if(gameObject.name == "FluidGaugeJ") x *= 9f; // percents across the screen
+      if(gameObject.name == "FluidGaugeK") x *= 12f;
+      else if(gameObject.name == "FluidGaugeL") x *= 15f;
+      float y = 10f * 0.01f * cam.pixelHeight;
+      float z = 5f;
 
-        Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(x, y, cam.nearClipPlane));
-        transform.position = new Vector3(worldPoint.x, worldPoint.y, z);
-      }
-      else {
-        // gauge disappears
-        transform.localPosition = new Vector3(-100f, -100f, 100f);
-      }
+      Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(x, y, cam.nearClipPlane));
+      transform.position = new Vector3(worldPoint.x, worldPoint.y, z);
     }
 }
