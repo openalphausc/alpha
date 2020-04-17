2c37386585319ac62805bc81c6c727d44c8c87e1
Owen Mech <mech.owen@gmail.com>
2020-04-17 16:51:16 -0500

    Feedback Email Implemented

---
+++ Assets/Scripts/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feedback : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendFeedback()
+    {
+        Application.OpenURL("mailto:openalpha@usc.edu?subject=Wishy%20Washy%20Feedback&body=Device:%20" + SystemInfo.deviceModel + "%0A" + "OS:%20" + SystemInfo.operatingSystem + "%0A%0A%0A%0A");
+    }
+}
