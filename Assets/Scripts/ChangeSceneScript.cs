36a9d38def052ce18e84c0cb448e5ea184db53c0
Colin Spiridonov <colinspiri@gmail.com>
2020-04-17 20:08:54 -0700

    Merged from title screen

---
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    public string sceneName;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
