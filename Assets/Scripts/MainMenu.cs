d9301976e123dca9a7c3c9764fc51e630c75e5b4
Charlie Feuerborn <cfeuerbo@usc.edu>
2020-04-24 02:29:39 -0700

    Added settings menu and slider

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class MainMenu : MonoBehaviour
+{
+    [FormerlySerializedAs("MainMenuUI")] public GameObject mainMenuUi;
+
+    [FormerlySerializedAs("SettingsMenuUI")] public GameObject settingsMenuUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SaveLoader.LoadGame();
+        SceneManager.LoadScene("TutorialScene");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting ...");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("TutorialScene");
+        }
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenuUi.SetActive(true);
+        mainMenuUi.SetActive(false);
+    }
+    
+    public void ReturnToMain()
+    {
+        settingsMenuUi.SetActive(false);
+        mainMenuUi.SetActive(true);
+    }
+}
