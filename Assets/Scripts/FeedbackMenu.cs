48323b4b83e5229943f1524ece0895ee410bbe15
Charlie Feuerborn <cfeuerbo@usc.edu>
2020-04-24 04:16:09 -0700

    Added feedbackmenuui script and separate feedback screen from help

---
+++ Assets/Scripts/FeedbackMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject helpMenuUI;
+
+    public GameObject feedbackMenuUI;
+
+    public AudioMixer masterMixer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SaveLoader.LoadGame();
+        SceneManager.LoadScene("TutorialScene");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting ...");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToMain();
+        }
+    }
+
+    public void OpenFeedback()
+    {
+        feedbackMenuUi.SetActive(true);
+        helpMenuUi.SetActive(false);
+    }
+    
+    public void ReturnToHelp()
+    {
+        feedbackMenuUi.SetActive(false);
+        helpMenuUi.SetActive(true);
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        masterMixer.SetFloat("masterVolume", volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+        Debug.Log(volume);
+        masterMixer.SetFloat("musicVolume", volume);
+    }
+    
+    public void SetSFXVolume(float volume)
+    {
+        Debug.Log(volume);
+        masterMixer.SetFloat("sfxVolume", volume);
+    }
+}
