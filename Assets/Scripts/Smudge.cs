1f43c87258fc966a0a944f2bdd702d62a6aac5d7
David Barrett <DLMBarrett@gmail.com>
2020-04-24 16:29:56 -0700

    Helper Text First Pass
    added a child text ui object that changes based on the type of the smudge

--- Assets/Scripts/Smudge.cs
+++ Assets/Scripts/Smudge.cs
@@ -20,8 +20,12 @@ public class Smudge : MonoBehaviour
     public float percentNeutralized = 0; // 0 means not neutralized, 100 means neutralized, but it's a gradient
     public bool neutralized = false;
     public bool selected = false;
+    public GameObject FloatingTextPrefab;
 
     private new SpriteRenderer renderer;
+
+    private Vector3 SmudgePosition;
+    private Quaternion SmudgeRotation;
 
     private float startScale = 0.7f;
 
@@ -30,6 +34,13 @@ public class Smudge : MonoBehaviour
         renderer = GetComponent<SpriteRenderer>();
 
         transform.localScale = new Vector3(startScale, startScale, startScale);
+
+        SmudgePosition = gameObject.transform.position;
+        SmudgeRotation = gameObject.transform.rotation;
+        // Scene currentScene = SceneManager.GetActiveScene();
+        // if(currentScene.name == "TutorialScene") {
+        ShowFloatingText();
+        // }
     }
 
     void Update()
@@ -44,6 +55,27 @@ public class Smudge : MonoBehaviour
       float scalePercent = 100 - percentNeutralized;
       if(scalePercent <= 40) scalePercent = 50;
       transform.localScale = new Vector3(startScale * scalePercent/100, startScale * scalePercent/100, startScale * scalePercent/100);
+    }
+
+    void ShowFloatingText() 
+    {
+        GameObject helpUI = Instantiate(FloatingTextPrefab, SmudgePosition, SmudgeRotation, gameObject.transform);
+        helpUI.transform.position -= new Vector3(0, 0, 1);
+        string helperText = "";
+        switch(this.type) {
+            case SmudgeType.SmudgeJ : 
+                helperText = "J";
+                break;
+            case SmudgeType.SmudgeK :
+                helperText = "K";
+                break;
+            case SmudgeType.SmudgeL :
+                helperText = "L";
+                break;
+            case SmudgeType.SmudgeNone :
+                break;
+        }
+        helpUI.GetComponent<TextMesh>().text = helperText;
     }
 
     public void Select()
