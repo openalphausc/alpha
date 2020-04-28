c8ba657bd64dc87a4ee7d94c8e4ba8687ce9f12b
Colin Spiridonov <colinspiri@gmail.com>
2020-04-27 21:21:01 -0700

    Game pauses waiting for keyboard input on some barks

--- Assets/Scripts/TutorialInstructions.cs
+++ Assets/Scripts/TutorialInstructions.cs
@@ -7,11 +7,24 @@ public class TutorialInstructions : MonoBehaviour
 {
     public Text displayText;
 
+    private List<string> barks; // index is floor for both lists
+    private List<float> barkStatus; // 0 = done and gone, 1 = pause and waiting for keypress, 2 = pause and timing out soon, 3+ = not paused and timing out soon
+    private List<int> nextBark;
+    private float maxTimeOnScreen = 5f;
+    private int currentBark = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
       displayText = GetComponent<Text>();
-      displayText.text = "PRESS SPACE";
+      displayText.text = "INSTRUCTIONS HERE";
+
+      barks = new List<string>();
+      barkStatus = new List<float>();
+      nextBark = new List<int>();
+
+      LoadBarks();
     }
 
     // Update is called once per frame
@@ -19,19 +32,58 @@ public class TutorialInstructions : MonoBehaviour
     {
       int floor = FloorManager.floorIndex;
       bool moving = FloorManager.moving;
-      string text = "";
-      if(floor == 0) text = "PRESS SPACE";
-      else if(floor == 1 && moving) text = "GOOD";
-      else if(floor == 1 && !moving) text = "PRESS A AND D TO MOVE";
-      else if(floor == 2 && moving) text = "WELL DONE";
-      else if(floor == 2 && !moving) text = "CLEAN ALL 3 SMUDGES";
-      else if(floor == 3 && !moving) text = "SPRAY CLEANING FLUID WITH J";
-      else if(floor == 4 && !moving) text = "FLUID WILL START REFILLING AUTOMATICALLY";
-      else if(floor == 5 && moving) text = "YOUR CLEANING FLUID IS REFILLING.";
-      else if(floor == 5 && !moving) text = "WHEN IT'S CLOSE TO FULL, STOP IT WITH J";
-      else if(floor == 6 && moving) text = "IF YOU LET YOUR FLUID OVERFLOW,";
-      else if(floor == 6 && !moving) text = "YOU'LL HAVE TO STOP AND CLEAN UP THE MESS";
-      else if(floor == 7 && !moving) text = "NOW FINISH THE BUILDING";
-      displayText.text = text;
+      if(floor == 1 && !moving) currentBark = 4;
+      if(floor == 2 && !moving) currentBark = 5;
+      if(floor == 3 && !moving) currentBark = 6;
+
+
+      ShowBark(currentBark);
+    }
+
+    void ShowBark(int i) {
+      if(barkStatus[i] == 0) {
+        displayText.text = "";
+        if(nextBark[i] != -1) currentBark = nextBark[i];
+        return;
+      }
+
+      displayText.text = barks[i];
+      if(barkStatus[i] == 1 || barkStatus[i] == 2) Time.timeScale = 0f; // pause world
+      print("showing bark for currentBark = " + i);
+
+      if(barkStatus[i] == 1 && (i == 3 && Input.GetKeyDown(KeyCode.Space)
+      || i == 4 && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+      || i == 6 && Input.GetKeyDown(KeyCode.J)
+      )) {
+        barkStatus[i] = 0;
+        Time.timeScale = 1f; // unpause world
+      }
+      else if(barkStatus[i] >= 2) {
+        barkStatus[i] += Time.unscaledDeltaTime;
+        if(barkStatus[i] > maxTimeOnScreen) {
+          Time.timeScale = 1f; // unpause world
+          barkStatus[i] = 0;
+        }
+      }
+    }
+
+    void LoadBarks() {
+      barks.Add("You're real new to this, huh?");
+      barkStatus.Add(2); nextBark.Add(1);
+      barks.Add("You know what? I think I'll call you Squeegee.");
+      barkStatus.Add(2); nextBark.Add(2);
+      barks.Add("ALRIGHT! Listen up 'cause I only want to explain this once.");
+      barkStatus.Add(2); nextBark.Add(3);
+      barks.Add("You see those disgusting smudges? Press 'SPACE' to wipe 'em.");
+      barkStatus.Add(1); nextBark.Add(-1);
+
+      barks.Add("You want to move? Press 'A' and 'D'.");
+      barkStatus.Add(1); nextBark.Add(-1);
+
+      barks.Add("Now, spray those smudges and get crackin'.");
+      barkStatus.Add(3); nextBark.Add(-1);
+
+      barks.Add("To spray smudges, press 'J'.");
+      barkStatus.Add(1); nextBark.Add(-1);
     }
 }
