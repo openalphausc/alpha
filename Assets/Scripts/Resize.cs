e442a6fbaa067ccae391bd0f5b92bd73753d4dd1
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-05-02 14:10:55 -0500

    Tried Resizing

---
+++ Assets/Scripts/Resize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(worldScreenWidth, worldScreenHeight, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
