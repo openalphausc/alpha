9f286769587831fd22bbb30a139d13d129dec1aa
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-05-02 02:45:07 -0500

    Subway Volcano Scene Implemented

---
+++ Assets/Scripts/SubwayScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubwayScript : MonoBehaviour
+{
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 10.0f)
+        {
+            SceneManager.LoadScene("NewspaperScene");
+        }
+    }
+}
