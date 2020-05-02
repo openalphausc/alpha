7e372294bdc645496a6df2a0f055ee0bb0f161fb
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-05-02 05:38:22 -0500

    Subway animations random

--- Assets/Scripts/SubwayScript.cs
+++ Assets/Scripts/SubwayScript.cs
@@ -6,10 +6,26 @@ using UnityEngine.SceneManagement;
 public class SubwayScript : MonoBehaviour
 {
     float time;
+    public GameObject volcano;
+    public GameObject squid;
+    public GameObject spag;
     // Start is called before the first frame update
     void Start()
     {
-        time = 0.0f;   
+        time = 0.0f;
+        int number = Random.Range(1, 4);
+        if(number == 1)
+        {
+            Instantiate(volcano);
+        }
+        if (number == 2)
+        {
+            Instantiate(squid);
+        }
+        if (number == 3)
+        {
+            Instantiate(spag);
+        }
     }
 
     // Update is called once per frame
