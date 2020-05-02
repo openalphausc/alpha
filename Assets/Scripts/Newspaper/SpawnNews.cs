60b76d75c3a6d8ae631faa7f1545fec98db8e5a2
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-05-02 02:24:56 -0500

    Prefabs Made

--- Assets/Scripts/Newspaper/SpawnNews.cs
+++ Assets/Scripts/Newspaper/SpawnNews.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class SpawnNews : MonoBehaviour
 {
     float percent;
+    public GameObject parent;
+    public GameObject volcano;
+    public GameObject squid;
+    public GameObject sMonster;
     // Start is called before the first frame update
     void Start()
     {
         percent = PersistentManagerScript.Instance.levelProgress;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject news;
+        int number = Random.Range(1, 3);
+        if(number == 1)
+        {
+            news = Instantiate(volcano, parent.transform);
+        }
+        if (number == 2)
+        {
+            news = Instantiate(squid, parent.transform);
+        }
+        if (number == 3)
+        {
+            news = Instantiate(sMonster, parent.transform);
+        }
     }
 }
